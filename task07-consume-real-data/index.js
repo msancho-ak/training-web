b03e45c05b99517b2cb5e3c10df3eae0056a6d88
msancho-ak <106824234+msancho-ak@users.noreply.github.com>
2022-06-27 16:32:22 -0600

    Changes requested

--- task07-consume-real-data/index.js
+++ task07-consume-real-data/index.js
@@ -1,19 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    paintData();    
-});
-
-function readMoreOrLess(){
+document.addEventListener("DOMContentLoaded", () => paintData());
+ 
+const readMoreOrLess = () =>{
     let readMoreDiv = document.getElementById("readMoreSipnosis")
     let readMoretext = document.getElementById("readMoreText")
     let readMoreIcon = document.getElementById("readMoreIcon")
     let logoReadMore = document.getElementById("logoReadMore")
-    
     if(window.getComputedStyle(readMoreDiv).display == 'none'){
         logoReadMore.style.display = 'block';
         readMoreDiv.style.display = 'block';
         readMoretext.innerText = "Read Less"
         readMoreIcon.src ="images/readLess.svg"
-
     }
     else{
         logoReadMore.style.display = 'none';
@@ -21,10 +17,9 @@ function readMoreOrLess(){
         readMoretext.innerText = "Read All"
         readMoreIcon.src ="images/readMore.svg"
     }
-    
 }
 
-function showOrHideMenu(){
+const showOrHideMenu = () => {
 
     let menuOptions = document.getElementById("menuOptions")
     let iconMenu = document.getElementById("iconMenu")
@@ -37,7 +32,7 @@ function showOrHideMenu(){
         iconMenu.src ="images/menu.svg"
     }
 }
-async function readRealData() {
+const  readRealData = async () => {
     try {
         let response =  await fetch("https://static.akurey.com/trainings/OnePieceInformation.json");
         if (!response.ok) {
@@ -50,94 +45,85 @@ async function readRealData() {
         console.error(`Could not get data: ${error}`);
     }
 }
-function compareStrings(a, b) {
+const compareStrings =(a, b) => {
     return (a < b) ? -1 : (a > b) ? 1 : 0;
 }
 
 const paintData = ()=>{
     const json = readRealData();
     json.then((result)=>{
-        let sortedIslands = result.islands.sort(function(a, b) {
-            return compareStrings(a.name, b.name);
-        })
-        let sortedCharacters = result.characters.sort(function(a, b) {
-            return compareStrings(a.name, b.name);
-        })
-        let sortedMysticObjects = result.mysticObjects.sort(function(a, b) {
-            return compareStrings(a.name, b.name);
-        })
-        populateCharacters(sortedCharacters);
-        populateIslands(sortedIslands);
-        populateMysticObjects(sortedMysticObjects);
+        let sortedIslands = result.islands.sort((a, b) =>compareStrings(a.name, b.name));
+        let sortedCharacters = result.characters.sort((a, b) =>compareStrings(a.name, b.name));
+        let sortedMysticObjects = result.mysticObjects.sort((a, b) =>compareStrings(a.name, b.name));
+        populateData(sortedCharacters,1);
+        populateData(sortedIslands,2);
+        populateData(sortedMysticObjects,3);
     })
 }
 
-
-
-const populateCharacters = (charactersList)=>{
-    let  charactersGrid = document.getElementById('characters-grid');
-    let charactersListSlice = charactersList.slice(0,4);
-    charactersListSlice.forEach((element,index) => {
-        let html = charactersHTML(element);
-        charactersGrid.innerHTML = charactersGrid.innerHTML + html;
+const populateData = (list,type)=>{
+    let grid = null;
+    let listSliced = null;
+    if(type === 1){
+        grid = document.getElementById('characters-grid');
+        listSliced = list.slice(0,4);
+    }
+    else if(type === 2){
+        grid = document.getElementById('island-grid');
+        listSliced = list.slice(0,3);
+    }
+    else{
+        grid = document.getElementById('mysticObjects-grid');
+        listSliced = list.slice(0,4);
+    }
+    listSliced.forEach((element,index) => {
+        let html = getHTMLStructure(element, type);
+        grid.innerHTML = grid.innerHTML + html;
     });
 }
-const charactersHTML = (character) => {
-    const htmlStructure = `<div class="characters-container_grid_item">
-                                <img src="images/${character.img}" alt="" />
+
+const getHTMLStructure = (elem,type) =>{
+    if(type === 1){
+        const htmlStructure = `<div class="characters-container_grid_item">
+                                <img src="images/${elem.img}" alt="" />
                                 <p class="characters-container_grid_item_characterName">
-                                    ${character.name}
+                                    ${elem.name}
                                 </p>
                             </div>`
-    return htmlStructure;
-}
-
-
-const populateIslands = (islandsList)=>{
-    let  islandGrid = document.getElementById('island-grid');
-    let islandListSlice = islandsList.slice(0,3);
-    islandListSlice.forEach((element,index) => {
-        let html = islandsHTML(element);
-        islandGrid.innerHTML = islandGrid.innerHTML + html;
-    });
-}
-
-
-const islandsHTML = (island) => {
+     return htmlStructure;
+    }
+    else if(type ===2){
         const htmlStructure = `<div class="island-container_grid_item">
-                                    <img src="images/${island.img}" alt="" />
+                                    <img src="images/${elem.img}" alt="" />
                                     <div class="island-container_grid_item_descripcion">
-                                    <b><span>${island.name}</span></b>
+                                    <b><span>${elem.name}</span></b>
                                     <div class="island-container_grid_item_descripcion_region">
                                         <img
                                         style="width: 10px; height: 10px"
                                         src="images/loc.svg"
                                         alt=""
                                         />
-                                        <span>${island.Location}</span>
+                                        <span>${elem.Location}</span>
                                     </div>
                                     </div>
                                 </div>`
-    return htmlStructure;
-}
-
-
-const populateMysticObjects = (mysticObjectsList)=>{
-    let  mysticObjectsGrid = document.getElementById('mysticObjects-grid');
-    let mysticObjectsSlice = mysticObjectsList.slice(0,4);
-    mysticObjectsSlice.forEach((element) => {
-        let html = mysticObjectsHTML(element);
-        mysticObjectsGrid.innerHTML = mysticObjectsGrid.innerHTML + html;
-    });
-}
-
-const mysticObjectsHTML = (object) => {
-    const htmlStructure = `<div class="mist-objects-container_grid_item">
+        return htmlStructure;
+    }
+    else{
+        const htmlStructure = `<div class="mist-objects-container_grid_item">
                             <img src="images/gommu.svg" alt="" />
                             <div class="mist-container_grid_item_descripcion">
-                            <b><span>${object.name}</span></b> 
-                            <span>${object.description.length > 190 ? object.description.substring(0,191)+"...":object.description }</span>
+                            <b><span>${elem.name}</span></b> 
+                            <span>${elem.description.length > 190 ? elem.description.substring(0,191)+"...":elem.description }</span>
                             </div>
                         </div>`
-return htmlStructure;
+        return htmlStructure;
+
+    }
 }
+
+
+
+
+
+
